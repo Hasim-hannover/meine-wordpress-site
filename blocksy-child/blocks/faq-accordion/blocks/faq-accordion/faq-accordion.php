d59c7fe56cac5df368fd3a712e2a7aa3d2eed8a0
domdar <hasimster@gmail.com>
2025-09-19 01:04:39 +0200

    faq php

--- blocksy-child/blocks/faq-accordion/blocks/faq-accordion/faq-accordion.php
+++ blocksy-child/blocks/faq-accordion/blocks/faq-accordion/faq-accordion.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Block Template: FAQ Accordion
+ * Finale & robusteste Version
  */
 
 // Block-spezifische Klassen und IDs generieren
@@ -9,14 +10,18 @@ $class_name = 'faq-accordion-block';
 if (!empty($block['className'])) {
     $class_name .= ' ' . $block['className'];
 }
+
+// **DIE ENTSCHEIDENDE ÄNDERUNG**: Die Feld-Daten explizit für diesen Block laden
+$faq_items = get_field('faq_items');
+
 ?>
 
 <div id="<?php echo esc_attr($block_id); ?>" class="<?php echo esc_attr($class_name); ?>">
-    <?php if (have_rows('faq_items')) : ?>
+    <?php if ( $faq_items ) : ?>
         <div class="faq">
-            <?php while (have_rows('faq_items')) : the_row();
-                $question = get_sub_field('question');
-                $answer = get_sub_field('answer');
+            <?php foreach ( $faq_items as $item ) :
+                $question = $item['question'];
+                $answer = $item['answer'];
             ?>
                 <details>
                     <summary><?php echo esc_html($question); ?></summary>
@@ -24,9 +29,11 @@ if (!empty($block['className'])) {
                         <?php echo wp_kses_post($answer); ?>
                     </div>
                 </details>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </div>
     <?php else : ?>
-        <p>Bitte fügen Sie im Editor Fragen und Antworten hinzu.</p>
+        <p style="color: #ff8a00; padding: 2rem; text-align: center;">
+            [Vorschau: FAQ Block] – Bitte fügen Sie im Editor (rechte Seitenleiste) Fragen und Antworten hinzu.
+        </p>
     <?php endif; ?>
 </div>
