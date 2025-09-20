625c99ec7eddb819422378edce13aa355f19ddb2
domdar <hasimster@gmail.com>
2025-09-20 21:04:58 +0200

    ok

--- blocksy-child/inc/theme-setup.php
+++ blocksy-child/inc/theme-setup.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Theme Setup: Einbinden von Styles und Skripten.
+ * FINALE VERSION mit korrigierter Lade-Priorität zur Behebung von FOUC.
  *
  * @package Blocksy Child
  */
@@ -10,27 +11,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Lädt die CSS- und JS-Dateien des Child-Themes.
- * Diese Funktion wird über einen Hook in WordPress aufgerufen.
+ * Lädt die JavaScript-Dateien des Child-Themes.
  */
 function ct_enqueue_assets() {
-    // Lädt das dedizierte Stylesheet für die Startseite nur auf der Startseite.
+    // Lädt das dedizierte JavaScript für die Startseite.
     if ( is_front_page() ) {
-        wp_enqueue_style(
-            'blocksy-child-homepage',
-            get_stylesheet_directory_uri() . '/assets/css/homepage.css',
-            [],
-            filemtime( get_stylesheet_directory() . '/assets/css/homepage.css' )
-        );
-
-        // Lädt das dedizierte JavaScript für die Startseite und behebt den PageSpeed-Fehler.
         wp_enqueue_script(
             'blocksy-child-homepage',
             get_stylesheet_directory_uri() . '/assets/js/homepage.js',
             [],
             filemtime( get_stylesheet_directory() . '/assets/js/homepage.js' ),
-            true
+            true // Lädt das Skript im Footer.
         );
     }
 }
-add_action( 'wp_enqueue_scripts', 'ct_enqueue_assets', 15 );
+add_action( 'wp_enqueue_scripts', 'ct_enqueue_assets' );
+
+
+/**
+ * Fügt das CSS der Startseite mit HÖCHSTER PRIORITÄT in den <head> ein.
+ * Das verhindert das "Aufblitzen" von ungestyltem Inhalt (FOUC).
+ */
+function hu_inline_homepage_styles() {
+    // Führt diesen Code nur auf der Startseite aus.
+    if ( is_front_page() ) {
+        $css_path = get_stylesheet_directory() . '/assets/css/homepage.css';
+        if ( file_exists( $css_path ) ) {
+            // Liest den Inhalt der CSS-Datei.
+            $css_content = file_get_contents( $css_path );
+            // Gibt das CSS direkt in einem <style>-Tag aus.
+            echo '<style id="hu-homepage-inline-styles">' . $css_content . '</style>';
+        }
+    }
+}
+// Die Priorität "1" sorgt dafür, dass dieser Code ganz am Anfang der Warteschlange ausgeführt wird.
+add_action( 'wp_head', 'hu_inline_homepage_styles', 1 );
