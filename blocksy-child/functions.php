6a38607f58779cde2efb11dd222fefe6b034f3d3
domdar <hasimster@gmail.com>
2025-09-20 02:45:30 +0200

    ok

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -15,7 +15,24 @@ function blocksy_child_enqueue_styles() {
 
 /**
  * Custom Theme Setup
+ * Lädt Skripte, Styles und andere Theme-Konfigurationen.
  */
 if ( file_exists( __DIR__ . '/inc/theme-setup.php' ) ) {
     require_once __DIR__ . '/inc/theme-setup.php';
+}
+
+
+/**
+ * NEU: Lädt die Shortcodes für die Startseite.
+ * Ohne diesen Teil weiß WordPress nichts von den Shortcodes.
+ */
+$shortcodes_path = get_stylesheet_directory() . '/inc/shortcodes.php';
+
+if ( file_exists( $shortcodes_path ) ) {
+    require_once $shortcodes_path;
+} else {
+    // Zeigt eine Fehlermeldung im Backend an, falls die Datei fehlt.
+    if ( current_user_can('manage_options') ) {
+        wp_die('FEHLER: Die Datei /inc/shortcodes.php konnte nicht gefunden werden. Bitte Pfad prüfen!');
+    }
 }
