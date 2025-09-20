5ebadcc2dd9ef9d58fcbe62a7e86ddc839c01161
domdar <hasimster@gmail.com>
2025-09-20 18:32:24 +0200

    11

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -36,3 +36,30 @@ if ( file_exists( $shortcodes_path ) ) {
         wp_die('FEHLER: Die Datei /inc/shortcodes.php konnte nicht gefunden werden. Bitte Pfad prüfen!');
     }
 }
+/**
+ * Macht den Inhalt von der Startseite, der durch Shortcodes generiert wird,
+ * für die Rank Math SEO-Analyse sichtbar.
+ *
+ * @param string $content Der ursprüngliche Inhalt aus dem Editor (der leer ist).
+ * @return string Der neue Inhalt, der an Rank Math übergeben wird.
+ */
+add_filter( 'rank_math/frontend/builder/content', function( $content ) {
+    // Wir führen diesen Code nur auf der Startseite aus.
+    if ( ! is_front_page() ) {
+        return $content;
+    }
+
+    // Wir holen uns den Inhalt aus den Shortcode-Funktionen.
+    // WICHTIG: Die Reihenfolge hier muss der Reihenfolge auf deiner Seite entsprechen.
+    $custom_content = '';
+    $custom_content .= do_shortcode('[homepage_hero_shortcode]');
+    $custom_content .= do_shortcode('[homepage_partner_shortcode]');
+    $custom_content .= do_shortcode('[homepage_about_shortcode]');
+    $custom_content .= do_shortcode('[homepage_services_shortcode]');
+    $custom_content .= do_shortcode('[homepage_cta_shortcode]');
+    $custom_content .= do_shortcode('[homepage_faq_shortcode]');
+
+    // Wir entfernen alle HTML-Tags, damit Rank Math reinen Text analysiert.
+    // Und wir geben den zusammengebauten Inhalt zurück.
+    return strip_tags( $custom_content );
+});
