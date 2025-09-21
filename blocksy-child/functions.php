e829e4834b8d21f34eaa26264f52e17531424b2e
domdar <hasimster@gmail.com>
2025-09-21 22:49:44 +0200

    1

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -9,7 +9,6 @@
 add_action('wp_enqueue_scripts', 'hu_enqueue_theme_styles');
 function hu_enqueue_theme_styles()
 {
-    // Parent-Theme-Stylesheet
     wp_enqueue_style(
         'blocksy-parent-style',
         get_template_directory_uri() . '/style.css',
@@ -17,12 +16,11 @@ function hu_enqueue_theme_styles()
         wp_get_theme('blocksy')->get('Version')
     );
 
-    // Child-Theme-Stylesheet
     wp_enqueue_style(
         'blocksy-child-style',
         get_stylesheet_uri(),
         ['blocksy-parent-style'],
-        filemtime(get_stylesheet_directory() . '/style.css') // Cache-Busting
+        filemtime(get_stylesheet_directory() . '/style.css')
     );
 }
 
@@ -40,17 +38,16 @@ function hu_enqueue_homepage_assets()
                 'hu-homepage-style',
                 get_stylesheet_directory_uri() . '/assets/css/homepage.css',
                 [],
-                filemtime($css_path) // Cache-Busting
+                filemtime($css_path)
             );
         }
 
         if (file_exists($js_path)) {
-            wp_enqueue_style( 'wp-mediaelement' );
             wp_enqueue_script(
                 'hu-homepage-script',
                 get_stylesheet_directory_uri() . '/assets/js/homepage.js',
-                ['jquery', 'wp-mediaelement'],
-                filemtime($js_path), // WICHTIG: Cache-Busting!
+                [], // Keine Abhängigkeiten wie jQuery nötig -> effizienter
+                filemtime($js_path),
                 true
             );
         }
@@ -69,7 +66,7 @@ function hu_enqueue_blog_archive_assets()
                 'hu-blog-archive-style',
                 get_stylesheet_directory_uri() . '/assets/css/blog-archive.css',
                 [],
-                filemtime($css_path) // Cache-Busting
+                filemtime($css_path)
             );
         }
     }
@@ -80,7 +77,6 @@ function hu_enqueue_blog_archive_assets()
 add_action('wp_enqueue_scripts', 'hu_enqueue_single_post_assets');
 function hu_enqueue_single_post_assets()
 {
-    // Prüft, ob es ein einzelner Beitrag ist (aber keine Seite und kein Anhang)
     if (is_singular('post')) {
         $css_path = get_stylesheet_directory() . '/assets/css/blog-single.css';
         $js_path = get_stylesheet_directory() . '/assets/js/blog-single.js';
@@ -90,7 +86,7 @@ function hu_enqueue_single_post_assets()
                 'hu-single-post-style',
                 get_stylesheet_directory_uri() . '/assets/css/blog-single.css',
                 [],
-                filemtime($css_path) // Cache-Busting
+                filemtime($css_path)
             );
         }
 
@@ -99,7 +95,7 @@ function hu_enqueue_single_post_assets()
                 'hu-single-post-script',
                 get_stylesheet_directory_uri() . '/assets/js/blog-single.js',
                 [],
-                filemtime($js_path), // Cache-Busting
+                filemtime($js_path),
                 true
             );
         }
