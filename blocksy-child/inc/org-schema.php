b2cab27c451c4dfcc3951cb467e4adc8efd537d9
domdar <hasimster@gmail.com>
2026-01-14 02:24:27 +0100

    1

--- blocksy-child/inc/org-schema.php
+++ blocksy-child/inc/org-schema.php
@@ -2,55 +2,63 @@
 /**
  * NEXUS ORGANIZATION SCHEMA (JSON-LD)
  * Standort: Warschauer Straße, Pattensen
- * Zweck: Google Local & Knowledge Graph Dominanz
+ * Version: FINAL VALIDATED
  */
 
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 add_action( 'wp_head', function() {
-    // Nur auf der Startseite oder Kontaktseite ausgeben (vermeidet Duplikate)
+    // Nur auf Startseite & Kontaktseite feuern
     if ( ! is_front_page() && ! is_page('kontakt') ) {
         return;
     }
 
     $schema = [
         '@context'      => 'https://schema.org',
-        '@type'         => 'ProfessionalService', // Oder 'LocalBusiness'
+        '@type'         => 'ProfessionalService',
         '@id'           => home_url( '/#organization' ),
         'name'          => 'Hasim Üner | WordPress & Performance Marketing',
         'url'           => home_url(),
-        'logo'          => get_stylesheet_directory_uri() . '/assets/img/logo.png', // Pfad ggf. anpassen!
-        'image'         => get_stylesheet_directory_uri() . '/assets/img/og-image.jpg', // Dein Profilbild/Banner
+        
+        // --- DEINE DATEN (GEPRÜFT) ---
+        'telephone'     => '+49 176 81407134', 
+        'email'         => 'info@hasimuener.de', 
+        'image'         => 'https://hasimuener.de/wp-content/uploads/2024/10/Profilbild_Hasim-Uener.webp',
+        'logo'          => 'https://hasimuener.de/wp-content/uploads/2025/08/cropped-Logo-hasim-uener-1.webp',
+        // -----------------------------
+
         'description'   => 'Spezialist für High-Performance WordPress-Systeme, Tracking (GA4) und Conversion-Optimierung in Hannover & Pattensen.',
-        'founder'       => [
-            '@type' => 'Person',
-            'name'  => 'Hasim Üner'
-        ],
+        
         'address'       => [
             '@type'           => 'PostalAddress',
             'streetAddress'   => 'Warschauer Straße',
             'addressLocality' => 'Pattensen',
-            'postalCode'      => '30982', // PLZ für Pattensen
+            'postalCode'      => '30982',
             'addressCountry'  => 'DE',
             'addressRegion'   => 'Niedersachsen'
         ],
+        
         'geo'           => [
             '@type'     => 'GeoCoordinates',
-            'latitude'  => '52.264', // Ca. Koordinaten Pattensen (Zentrum)
+            'latitude'  => '52.264', 
             'longitude' => '9.761'
         ],
-        'contactPoint'  => [
-            '@type'       => 'ContactPoint',
-            'contactType' => 'customer service',
-            'areaServed'  => 'DE',
-            'availableLanguage' => ['German', 'English']
+        
+        'priceRange'    => '€€€',
+        
+        'openingHoursSpecification' => [
+            [
+                '@type' => 'OpeningHoursSpecification',
+                'dayOfWeek' => ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                'opens' => '09:00',
+                'closes' => '18:00'
+            ]
         ],
+        
         'sameAs'        => [
-            // Hier deine Social Media Links eintragen
-            'https://www.linkedin.com/in/hasim-%C3%BCner/',
-            'https://github.com/Hasim-hannover'
-        ],
-        'priceRange'    => '€€€'
+            'https://www.linkedin.com/in/hasim-uener/',
+            'https://github.com/hasimuener'
+        ]
     ];
 
     echo '<script type="application/ld+json">' . json_encode( $schema, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT ) . '</script>';
