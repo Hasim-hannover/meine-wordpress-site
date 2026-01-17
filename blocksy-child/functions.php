f1235ac7b91e28a82fd2b5062730b83aa1b7e98e
domdar <hasimster@gmail.com>
2026-01-17 19:18:50 +0100

    1

--- blocksy-child/functions.php
+++ blocksy-child/functions.php
@@ -82,3 +82,20 @@ add_action( 'wp_head', function () {
  * 2. Seiten (Typ "page") werden NICHT beeinflusst.
  */
 add_filter('blocksy:post_types:post:has_page_title', '__return_false');
+
+/**
+ * FIX: Titel-Steuerung für SEITEN (Page)
+ * Deaktiviert den Blocksy-Titel NUR auf spezifischen Landingpages,
+ * damit wir dort volle Design-Kontrolle haben.
+ */
+add_filter('blocksy:post_types:page:has_page_title', function ($has_title) {
+    
+    // Liste der Seiten-IDs oder Slugs, wo der Titel WEG soll:
+    // Beispiel: 'wordpress-agentur-hannover', 'seo-audit', 'kontakt'
+    if ( is_page( array( 'wordpress-agentur-hannover', 'seo-landingpage-slug-hier-einfuegen' ) ) ) {
+        return false;
+    }
+
+    // Auf allen anderen Seiten (Impressum etc.) bleibt der Titel da.
+    return $has_title;
+});
