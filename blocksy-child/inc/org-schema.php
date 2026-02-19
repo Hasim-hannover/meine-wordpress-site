81d44b75faf596693f5497eb76c6796a534254f0
domdar <hasimster@gmail.com>
2026-02-19 02:17:34 +0100

    1

--- blocksy-child/inc/org-schema.php
+++ blocksy-child/inc/org-schema.php
@@ -14,7 +14,7 @@ function hu_output_schema()
         '@context' => 'https://schema.org',
         '@type'    => 'LocalBusiness',
         '@id'      => home_url('/#organization'),
-        'name'     => 'Hasim Üner',
+        'name'     => 'Hasim Üner – Growth Architect',
         'alternateName' => 'Hasim Üner Webentwicklung & CRO',
         'url'      => home_url(),
         'description' => 'Spezialist für High-Performance WordPress-Systeme, Tracking (GA4) und Conversion-Optimierung in Hannover & Pattensen.',
