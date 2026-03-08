3f0b3de75fe0bae63f5e22821633e9f7d96e4186
domdar <hasimster@gmail.com>
2026-03-08 22:22:16 +0100

    Step6

--- blocksy-child/template-parts/site-footer.php
+++ blocksy-child/template-parts/site-footer.php
@@ -47,7 +47,7 @@ $privacy_url = nexus_get_page_url( [ 'datenschutz' ], home_url( '/datenschutz/' 
 	<div class="ft__top">
 		<div class="ft__brand">
 			<a class="ft__logo" href="<?php echo esc_url( $home_url ); ?>" aria-label="Startseite - Hasim Üner">Hasim Üner</a>
-			<p class="ft__tag">WordPress als Nachfrage-System für B2B - aus Hannover</p>
+			<p class="ft__tag">WordPress als Nachfrage-System für B2B.</p>
 			<a class="ft__cta" href="<?php echo esc_url( $audit_url ); ?>">Audit starten (0 €)</a>
 		</div>
 
